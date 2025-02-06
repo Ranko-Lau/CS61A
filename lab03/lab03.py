8288683b935fc4e99a15538a6ebaee4b49fcb29c
明月心 <rankolau@gmail.com>
2025-02-06 23:57:44 +0800

    lab03: Completed

--- lab03/lab03.py
+++ lab03/lab03.py
@@ -51,7 +51,7 @@ def close_list(s, k):
     >>> close_list(t, 2)  # 2, 3, 4, and 5 are all within 2 of their index
     [2, 4, 3, 5]
     """
-    return [___ for i in range(len(s)) if ___]
+    return [s[i] for i in range(len(s)) if abs(s[i]-i)<=k]
 
 
 from math import sqrt
@@ -67,7 +67,7 @@ def squares(s):
     >>> squares(seq)
     []
     """
-    return [___ for n in s if ___]
+    return [int(sqrt(n)) for n in s if sqrt(n)==round(sqrt(n))]
 
 
 def double_eights(n):
@@ -92,6 +92,9 @@ def double_eights(n):
     True
     """
     "*** YOUR CODE HERE ***"
+    if n%100==88:return True
+    elif n<100:return False
+    else: return (double_eights(n//10))
 
 
 def make_onion(f, g):
@@ -120,10 +123,10 @@ def make_onion(f, g):
     """
     def can_reach(x, y, limit):
         if limit < 0:
-            return ____
+            return False
         elif x == y:
-            return ____
+            return True
         else:
-            return can_reach(____, ____, limit - 1) or can_reach(____, ____, limit - 1)
+            return can_reach(f(x), y, limit - 1) or can_reach(g(x), y, limit - 1)
     return can_reach
 
